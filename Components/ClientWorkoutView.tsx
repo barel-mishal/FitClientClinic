9e4ad44a2ddce93e8973f3aa58f328723d66741b
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-03-23 01:25:35 +0200

    add component to show workouts

---
+++ Components/ClientWorkoutView.tsx
@@ -0,0 +1,13 @@
+import { ScrollView, StyleSheet, Text } from "react-native"
+
+const ClientWorkoutView = (workouts: any) => {
+    return <ScrollView>
+        <Text>hello workouts</Text>
+    </ScrollView>
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ClientWorkoutView;
--- Pages/private/Trainer/TrainerClient.tsx
+++ Pages/private/Trainer/TrainerClient.tsx
@@ -5,16 +5,24 @@ import { RootStackParamList } from "../../../App";
 import { useAuth } from "../../../Components/ContextComopnents/AuthContext";
 import ClientView from "../../../Components/ClientView";
 import databaseMethods from "../../../services/databaseMethods";
+import { useQuery } from "react-query";
+import ClientWorkoutView from "../../../Components/ClientWorkoutView";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'TrainerClient'>;
 
 const TrainerClient: React.FC<Props> = ({ navigation, route: { params: {id}} }) => {
     const auth = useAuth();
     if (!auth.user || auth.data.role !== "trainer") return <View></View>;
-    const workouts = databaseMethods.getAllClientWorkouts(id)
+    const { data: workouts, error, isLoading } = useQuery(
+        ['workouts', id], 
+        () => databaseMethods.getUserClientWorkouts(id),
+    );
     const c = auth.data.clients.find((c) => c.userId! === id);
     console.log(workouts)
-    return <ClientView client={c} />;
+    return <>
+    <ClientView client={c} />
+    <ClientWorkoutView workouts={workouts} />
+    </>;
 }
 
 export default TrainerClient;
