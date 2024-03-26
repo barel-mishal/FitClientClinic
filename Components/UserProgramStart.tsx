ea2b92b48333e59681c73753d77fb3588a2e77b4
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-03-26 15:17:11 +0200

    Add UserProgramStart component and refactor ClientWorkout component

---
+++ Components/UserProgramStart.tsx
@@ -0,0 +1,93 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React, { useEffect } from "react";
+
+import { Text, StyleSheet, View } from "react-native";
+import { RootStackParamList } from "../App";
+import databaseMethods from "../services/databaseMethods";
+import RenderProgramTrack, { ProgramState } from "./ProgramViewTrack";
+import LoadingComp from "./LoadingComp";
+
+
+export type ComponentState = {
+    data: null, 
+    state: "loading", 
+    error: null
+} | {
+    data: null, 
+    state: "error", 
+    error: {
+        message: string
+}} | {
+    data: ProgramState, 
+    state: "success", 
+    error: null
+};
+
+
+
+interface UserProgramProps {
+    programId: string;
+    trainerId: string;
+    userId: string;
+    navigation: NativeStackNavigationProp<RootStackParamList, "ClientWorkout", undefined>
+}
+
+const UserProgarmStart: React.FC<UserProgramProps> = ({programId, trainerId, userId, navigation}) => {
+    const [program, setProgram] = React.useState<ComponentState>();
+    useEffect(() => {
+        setProgram({
+            data: null,
+            state: "loading",
+            error: null,
+        });
+        databaseMethods
+        .getTrainerProgram(trainerId, programId)
+        .then((res) => {
+            const ps: Partial<ProgramState> = {
+                ...res,
+                state: "start",
+                workoutTime: "1s",
+                completedExercises: [] as string[],
+                clientId: userId!,
+                startTime: Date.now(),
+            };
+            setProgram({
+                data: ps as ProgramState,
+                state: "success",
+                error: null,
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            setProgram({
+                data: null,
+                state: "error",
+                error: {message: "An error occured while fetching the program"},
+            });
+        }).finally(() => {;
+    });
+    }, []);
+
+    return (
+        <>
+            {program?.state === "loading" && <View style={styles.container}><LoadingComp /></View>}
+            {program?.state === "error" && <Text>{program.error?.message}</Text>}
+            {program?.state === "success" && <RenderProgramTrack program={program.data} navigation={navigation} />}
+        </>
+    );
+        
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f0f9ff",   
+    },
+});
+
+export default UserProgarmStart;
+
+
--- Pages/private/Client/ClientWorkout.tsx
+++ Pages/private/Client/ClientWorkout.tsx
@@ -1,29 +1,12 @@
-import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useEffect } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, {  } from "react";
 
-import { Text, StyleSheet, View } from "react-native";
 import { RootStackParamList } from "../../../App";
-import databaseMethods from "../../../services/databaseMethods";
-import RenderProgramTrack, { ProgramState } from "../../../Components/ProgramViewTrack";
 import { useAuth } from "../../../Components/ContextComopnents/AuthContext";
-import LoadingComp from "../../../Components/LoadingComp";
+import UserProgarmStart from "../../../Components/UserProgramStart";
+import { View } from "react-native";
 
 export type PropsClientWorkout = NativeStackScreenProps<RootStackParamList, 'ClientWorkout'>;
-
-type ComponentState = {
-    data: null, 
-    state: "loading", 
-    error: null
-} | {
-    data: null, 
-    state: "error", 
-    error: {
-        message: string
-}} | {
-    data: ProgramState, 
-    state: "success", 
-    error: null
-};
 
 const ClientWorkout: React.FC<PropsClientWorkout> = ({navigation, route: {params: {programId, trainerId}}}) => {
     const u = useAuth();
@@ -31,68 +14,9 @@ const ClientWorkout: React.FC<PropsClientWorkout> = ({navigation, route: {params
     return <UserProgarmStart programId={programId} trainerId={trainerId} userId={u.user.uid} navigation={navigation} />;
 }
 
-interface UserProgramProps {
-    programId: string;
-    trainerId: string;
-    userId: string;
-    navigation: NativeStackNavigationProp<RootStackParamList, "ClientWorkout", undefined>
-}
 
-const UserProgarmStart: React.FC<UserProgramProps> = ({programId, trainerId, userId, navigation}) => {
-    const [program, setProgram] = React.useState<ComponentState>();
-    useEffect(() => {
-        setProgram({
-            data: null,
-            state: "loading",
-            error: null,
-        });
-        databaseMethods
-        .getTrainerProgram(trainerId, programId)
-        .then((res) => {
-            const ps: Partial<ProgramState> = {
-                ...res,
-                state: "start",
-                workoutTime: "1s",
-                completedExercises: [] as string[],
-                clientId: userId!,
-                startTime: Date.now(),
-            };
-            setProgram({
-                data: ps as ProgramState,
-                state: "success",
-                error: null,
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-            setProgram({
-                data: null,
-                state: "error",
-                error: {message: "An error occured while fetching the program"},
-            });
-        }).finally(() => {;
-    });
-    }, []);
 
-    return (
-        <>
-            {program?.state === "loading" && <View style={styles.container}><LoadingComp /></View>}
-            {program?.state === "error" && <Text>{program.error?.message}</Text>}
-            {program?.state === "success" && <RenderProgramTrack program={program.data} navigation={navigation} />}
-        </>
-    );
-        
-    
-}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f0f9ff",   
-    },
-});
 
 export default ClientWorkout;
 
